4ffd1d2d97f7180880d82b1e706ef6d686954ece
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-11-14 16:38:48 +0000

    Init model

---
+++ db/2_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 2, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table asd.
+--
+CREATE TABLE caeschema.asd (
+  hshc INTEGER ,
+CONSTRAINT hshc_PK PRIMARY KEY (hshc)
+ 
+);
+
+
+
